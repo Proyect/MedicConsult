f4b432cab4e318cc1983541be0f6f1aef8c90034
Ariel M Diaz <arieldiaz.sistemas@gmail.com>
2024-07-30 15:33:06 -0300

    first commit

--- history/views.py
+++ history/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
-    return render(request,"list.html")
+    return render(request,"list.html",{
+        'form' : UserCreationForm
+    })
